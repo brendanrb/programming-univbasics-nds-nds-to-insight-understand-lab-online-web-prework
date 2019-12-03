e135b30721948053400b80b2790ac7de0095edac
Brendan R Borgers <brendanborgers@gmail.com>
2019-12-03 15:13:17 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -4,8 +4,23 @@ require 'directors_database'
 
 def pretty_print_nds(nds)
   # Change the code below to pretty print the nds with pp
-  nil
+  pp directors_database
 end
 
+
+
 def print_first_directors_movie_titles
+  outer_results = [0]
+row_index = 0
+while row_index < spice_rack.count do
+  element_index = 0
+  inner_results = [0]
+  while element_index < directors_database[row_index].count do
+    if directors_database[row_index][element_index][0]
+      inner_results << directors_database[row_index][element_index]
+    end
+    element_index += 1
+  end
+  outer_results << inner_results
+  row_index += 1
 end
